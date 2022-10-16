88b0c3c185e88ff3a975068c8e571941bfe2a9d2
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-10-16 08:35:30 +0200

    Add Duration component

---
+++ src/component/duration/Duration.jsx
@@ -0,0 +1,163 @@
+import React from 'react'
+import StyledDuration from './Duration.styled';
+
+import {
+    ResponsiveContainer,
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Rectangle
+} from "recharts";
+
+import PropTypes from 'prop-types'
+
+
+const CustomTooltip=({active, payload})=>{
+    if (active) {
+        return (
+            <div className="customTooltip">
+                <p className="tooltipData">{`${payload[0].value} `}min</p>
+            </div>
+        );
+}
+return null;
+}
+
+
+const CustomCursor = ({points}) => {
+    return <Rectangle   fill="#000000"
+                        opacity={0.2}
+                        x={points[1].x}
+                        width={1000}
+                        height={300}
+                    />;
+};
+
+const Duration = ({data}) => {
+
+	const dataSessions = [
+            {
+                "day": 'L',
+                "sessionLength": 30
+            },
+            {
+                "day": 'M',
+                "sessionLength": 23
+            },
+            {
+                "day": 'M',
+                "sessionLength": 45
+            },
+            {
+                "day": 'J',
+                "sessionLength": 50
+            },
+            {
+                "day": 'V',
+                "sessionLength": 0
+            },
+            {
+                "day": 'S',
+                "sessionLength": 0
+            },
+            {
+                "day": 'D',
+                "sessionLength": 60
+            }
+        ]
+        
+    
+	
+  return (
+    <StyledDuration className='duration'>
+        <ResponsiveContainer width='100%' aspect={1}>
+                <LineChart  style={{backgroundColor: "#FF0000"}}
+                            width={258}
+                            height={263}
+                            data={dataSessions}
+                            margin={{
+                                top: 50,
+                                right: -2,
+                                left: -60,
+                                bottom: 10
+                            }}
+                        >    
+                    
+                    <CartesianGrid  vertical={false} 
+                                    horizontal={false}
+                                />
+                    <XAxis  dataKey="day"
+                            tickLine={false}
+                            fillOpacity={0.5} 
+                            style={{
+                                transform: 'scale(0.9)',
+                                transformOrigin: 'bottom'
+                            }}
+                            tick={{
+                                fill:"#FFFFFF",
+                                fontWeight:500,
+                                fontSize:12
+                            }} 
+                            tickMargin={10} 
+                            axisLine={false} 
+                            interval="preserveStartEnd"
+                        />
+                    <YAxis  axisLine={false}
+                            tickLine={false} 
+                            tick={false}
+                            domain={['dataMin - 5', 'dataMax + 5']}
+                        />
+                    <Tooltip    content={<CustomTooltip/>}
+                                cursor={<CustomCursor/>}
+                            />
+                    <Line   type="natural"
+                            dataKey="sessionLength" 
+                            stroke="#FFFFFF"
+                            dot={false}
+                            opacity={0.8}
+                            strokeWidth={2}
+                        />
+                    <text   className='title'
+                            x="12%"
+                            y="15%"
+                            width={147}
+                            height={48}
+                            textAnchor="start" 
+                            dominantBaseline="middle"
+                            fill="#FFFFFF"
+                            style={{
+                                fontWeight:500,
+                                opacity:0.5
+                            }}
+                        >
+                        Durée moyenne des
+                    </text>
+                    <text   className='title'
+                            x="12%"
+                            y="25%"
+                            width={147}
+                            height={48}
+                            textAnchor="start"
+                            dominantBaseline="middle" 
+                            fill="#FFFFFF"
+                            style={{
+                                fontWeight:500,
+                                opacity:0.5
+                            }}
+                        >
+                        sessions
+                    </text>
+                </LineChart>
+            </ResponsiveContainer>
+	</StyledDuration>
+  )
+}
+
+export default Duration
+
+// Duration.propTypes = {
+//     data: PropTypes.array.isRequired
+//   }
---
+++ src/component/duration/Duration.styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const StyledDuration = styled.div`
+    width: 280px;
+    height: 280px;
+    background-color: red;
+    border-radius: 5px;
+    overflow: hidden;
+
+    .customTooltip {
+        background-color: white;
+        width: 39px;
+        height: 25px;
+        border-style: none;
+    }
+
+    .tooltipData {
+        color: #000000;
+        font-weight: 500;
+        font-size: 8px;
+        text-align: center;
+        line-height: 24px;
+    } 
+
+    .title {
+        font-size: 14px;
+    }
+
+    @media (min-width: 1200px) {
+            width: 260px;
+            height: 260px;
+        }
+`
+
+export default StyledDuration
